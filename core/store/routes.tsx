d20222c629a9d6b4cea8e9038e12133716f083f0
Copi5336 <jason.han@tpisoftware.com>
2021-05-03 11:23:32 +0800

    checkin

---
+++ core/store/routes.tsx
@@ -0,0 +1,20 @@
+const routes = [
+  {
+    path: "/sandwiches",
+    component: Sandwiches,
+  },
+  {
+    path: "/tacos",
+    component: Tacos,
+    routes: [
+      {
+        path: "/tacos/bus",
+        component: Bus,
+      },
+      {
+        path: "/tacos/cart",
+        component: Cart,
+      },
+    ],
+  },
+];
